ee0b6150964318eabe001e75f79379598c1f765b
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-14 00:20:40 +0700

    Update ini-scbot.js

--- plugins/ini-scbot.js
+++ plugins/ini-scbot.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
         let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
-let str = `*SAYA NYARI ESCE CUMA DI GITHUB BANH*\nSilahkan follow github saya dibawah ,Terimakasih`
+let str = `*👤SAYA NYARI ESCE CUMA DI GITHUB BANH*\n\n_Silahkan Follow Github Saya Di Bawah ,Terimakasih_`
 conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/MendingTuru/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ], m)
