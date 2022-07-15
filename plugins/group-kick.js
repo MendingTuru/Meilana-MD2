e924b0b6e2ba2c87957bad3c494e3e246624937e
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-15 21:32:04 +0700

    Update group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -1,27 +1,20 @@
-let handler = async (m, { teks, conn, isOwner, isAdmin, args }) => {
-	if (!(isAdmin || isOwner)) {
-                global.dfail('admin', m, conn)
-                throw false
-                }
-  let ownerGroup = m.chat.split`-`[0] + "@s.whatsapp.net";
-  if(m.quoted){
-if(m.quoted.sender === ownerGroup || m.quoted.sender === conn.user.jid) return;
-let usr = m.quoted.sender;
-await conn.groupParticipantsUpdate(m.chat, [usr], "remove"); return;
-}
-  if (!m.mentionedJid[0]) throw `tag yang mau dikick`;
-  let users = m.mentionedJid.filter(
-    (u) => !(u == ownerGroup || u.includes(conn.user.jid))
-  );
-  for (let user of users)
-    if (user.endsWith("@s.whatsapp.net"))
-      await conn.groupParticipantsUpdate(m.chat, [user], "remove");
-};
+import { areJidsSameUser } from '@adiwajshing/baileys'
+let handler = async (m, { conn, participants }) => {
+    let users = m.mentionedJid.filter(u => !areJidsSameUser(u, conn.user.id))
+    let kickedUser = []
+    for (let user of users)
+        if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) {
+            const res = await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
+            kickedUser.concat(res)
+            await delay(1 * 1000)
+        }
+    m.reply(`Succes kick ${kickedUser.map(v => '@' + v.split('@')[0])}`, null, { mentions: kickedUser })
 
+}
 handler.help = ['kick @user']
 handler.tags = ['group']
 handler.command = /^(kic?k|remove|tendang|\-)$/i
-
+handler.admin = true
 handler.group = true
 handler.botAdmin = true
 
