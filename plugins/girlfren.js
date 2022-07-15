42d1dc3797d6e045c98a87fc78b0ae2a286f7da3
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-15 16:50:38 +0700

    Update girlfren.js

--- plugins/girlfren.js
+++ plugins/girlfren.js
@@ -5,10 +5,10 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
   let _type = (args[0] || '').toLowerCase()
 
 //------- NOMOR
-  let nowner = `${nomorgf.split`@`[0]}@s.whatsapp.net`
-  let teksnomor = `${htki} *GIRLS FRIENDS OWNER* ${htka}
-• @${nomorgf.split`@`[0]} •
-------- ${namegf} -------
+  let nowner = `${nomorbf.split`@`[0]}@s.whatsapp.net`
+  let teksnomor = `${htki} *BOYS FRIENDS OWNER* ${htka}
+• @${nomorbf.split`@`[0]} •
+------- ${namebf} -------
 📮 *Note:*
 • Tidak menerima save contact, Takut Pangeran Marah
 • Saya Cewe Pangeran berhak blockir tanpa alasan
@@ -17,13 +17,13 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 • No Telp`
 
 //------------ BIO
-let ppown = await conn.profilePictureUrl(nomorgf + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
-let teksbio = `${htki} *BIODATA GIRLS FRIENDS* ${htka}
-*ɴᴀᴍᴇ:* Meilana×፝֟͜×
-*ᴀɢᴇ:* 18ᵗʰ
+let ppown = await conn.profilePictureUrl(nomorbf + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
+let teksbio = `${htki} *BIODATA BOYS FRIENDS* ${htka}
+*ɴᴀᴍᴇ:* ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ×፝֟͜×
+*ᴀɢᴇ:* 20ᵗʰ
 *sᴛᴀᴛᴜs:* single 
-*ʙɪʀᴛʜᴅᴀʏ:* 04 Mei 2004
-*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Tanggerang
+*ʙɪʀᴛʜᴅᴀʏ:* 21 Feb 2002
+*ᴀᴅᴅʀᴇss:* Indonesia, Jawa Barat, Jakarta
 *- - sᴋɪʟʟs: - -* 
 > JavaScript [89.7%]
 > Python [13.4%]
@@ -31,21 +31,21 @@ let teksbio = `${htki} *BIODATA GIRLS FRIENDS* ${htka}
 > Html [1.2%]
 📷 *Instagram:* ${sig2}
 🐈 *Github:* ${sgh2}
-🥏 *Whatsapp* wa.me/${nomorgf}
+🥏 *Whatsapp* wa.me/${nomorbf}
 `
   let teks = ' '
 const sections = [
    {
-	title: `${htjava} GIRLS FRIENDS OWNER –––––––––·•`,
+	title: `${htjava} BOYS FRIENDS OWNER –––––––––·•`,
 	rows: [
-	    {title: "📱 • Nomor", rowId: ".gfowner nomor"},
-	{title: "🎨 • Biodata", rowId: ".gfowner bio"},
+	    {title: "📱 • Nomor", rowId: ".bfowner nomor"},
+	{title: "🎨 • Biodata", rowId: ".bfowner bio"},
 	{title: "🌎 • Script", rowId: ".sc"},
 	]
     },{
 	title: `${htjava} SUPPORT ME –––––––·•`,
 	rows: [
-	    {title: "💹 • Donasi", rowId: ".gfowner nomor"},
+	    {title: "💹 • Donasi", rowId: ".bfowner nomor"},
 	{title: "🔖 • Sewa", rowId: ".sewabot"},
 	{title: "🌟 • Buy Premium", rowId: ".premium"},
 	]
@@ -55,20 +55,20 @@ const sections = [
 const listMessage = {
   text: teks,
   footer: null,
-  title: `${htki} *GIRLS FRIENDS OWNER* ${htka}`,
+  title: `${htki} *BOYS FRIENDS OWNER* ${htka}`,
   buttonText: "Click Here !",
   sections
 }
 
   try {
-    if (/(gfcreator|gfowner)/i.test(command)) {
+    if (/(bfcreator|bfowner)/i.test(command)) {
       const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
         switch (type) {
           case 'nomor':
           conn.reply(m.chat, teksnomor, m, { contextInfo: { mentionedJid: [nowner] }})
             break
             case 'bio':
-          conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorgf, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
+          conn.sendHydrated(m.chat, teksbio, wm, ppown, "wa.me/" + nomorbf, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], m)
             break
             
           default:
@@ -91,8 +91,8 @@ const listMessage = {
   }
 }
 
-handler.help = ['gfowner', 'gfcreaor']
+handler.help = ['bfowner', 'bfcreaor']
 handler.tags = ['main', 'info']
-handler.command = /^(gfowner|gfcreator)/i
+handler.command = /^(bfowner|bfcreator)/i
 
 export default handler
