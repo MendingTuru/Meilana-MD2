74cde62962d3d02f40c71a6d4797cfe2091174bd
ᵈʳᴋᴏᴋᴏ ᴘᴀ፝֟፝֟ɴɢᴇʀᴀɴ <104513437+MendingTuru@users.noreply.github.com>
2022-07-15 14:05:44 +0700

    Update sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -33,6 +33,5 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 handler.help = ['wm <packname>|<author>']
 handler.tags = ['sticker']
 handler.command = /^(wm)$/i
-handler.premium = true
 handler.limit = true
 export default handler
